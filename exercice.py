7742e447ec04f7eccd8e573a744de307c7eb148e
Dima <dumitru.zlotea@polymtl.ca>
2021-09-23 12:34:13 -0400

    exercices finis avec 1 erreur

--- exercice.py
+++ exercice.py
@@ -11,18 +11,56 @@ def get_num_letters(text):
 	return nb_letters
 
 def get_word_length_histogram(text):
-	return [0]
+	lettre_max = 0
+
+	for word in text.split():
+		if lettre_max < get_num_letters(word):
+			lettre_max = get_num_letters(word)
+	histogramme = [0] * (lettre_max + 1)
+
+	for word in text.split():
+		histogramme[get_num_letters(word)] += 1
+
+	return histogramme
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
+	affichage = ""
+	histogram = histogram[1:]
+	len_max = len(str(len(histogram[1:])))
 
-	return ""
+	for index, value in enumerate(histogram):
+		affichage += f"{(index + 1) : > 2}" + " " + ROW_CHAR * value + "\n"
+
+	return affichage
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	ligne1 = ""
+	ligne2 = ""
+	ligne3 = ""
+	ligne4 = LINE_CHAR * len(histogram)
 
-	return ""
+	for index, value in enumerate(histogram[1:]):
+		if value == 3:
+			ligne1 += BLOCK_CHAR
+		else:
+			ligne1 += " "
+
+		if value == 2 or value == 3:
+			ligne2 += BLOCK_CHAR
+		else:
+			ligne2 += " "
+
+		if value == 1 or value == 2 or value == 3:
+			ligne3 += BLOCK_CHAR
+		else:
+			ligne3 += " "
+
+	resultat = ligne1 + "\n" + ligne2 + "\n" + ligne3 + "\n" + ligne4
+
+	return resultat
 
 
 if __name__ == "__main__":
